f5041c8387541d874f174f01fb8c987e6162553d
Monica Piras <monica.piras.info@gmail.com>
2020-08-27 11:40:36 +0200

    update table material component

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,13 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
 
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
+
+Vue.use(VueMaterial);
+
+
 export default {
   components: {
     NavigationComponent
--- src/views/ContactsPage.vue
+++ src/views/ContactsPage.vue
@@ -1,50 +1,29 @@
 <template>
-  <div class="">
-    <h1>This is an Contacts page</h1>
-
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">FirstName</th>
-      <th scope="col">LastName</th>
-      <th scope="col">Gender</th>
-      <th scope="col">Edit</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>Male</td>
-      <td><button>Edit</button></td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>Male</td>
-      <td><button>Edit</button></td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>Male</td>
-      <td><button>Edit</button></td>
-    </tr>
-  </tbody>
-</table>
+  <div class="page-style">
+    <h1>Contacts</h1>
+    <table-search-component/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import TableSearchComponent from "@/components/TableSearchComponent.vue";
+
 export default {
-  name: 'ContactsPage',
+  name: "ContactsPage",
   components: {
+    TableSearchComponent
   }
-}
+};
 </script>
+<style lang="scss" scoped>
+h1 {
+  text-align: left;
+}
+.page-style {
+  padding: 20px;
+}
+
+</style>
+
