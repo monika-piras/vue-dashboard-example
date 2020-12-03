e6d06b50f4ae9c8fd6abbdae40df7d18c90750be
Monica Piras <monica.piras.info@gmail.com>
2020-12-03 11:42:16 +0100

    update chart bar and age on file json

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,15 @@
 
     <!-- <button @click="handleOnclick()">Promise</button> -->
 
-    <template>
-      <div>
-        <apexchart @click="handleOnclick()" width="380" type="donut" :options="chartData.chartOptions" :series="chartData.series"></apexchart>
-      </div>
-    </template>
+    <div>
+      <h4 class="titleChartStyle">Chart User's Gender</h4>
+      <apexchart @click="handleOnclick()" width="500" type="bar" :options="chartDataBar.chartOptions" :series="chartDataBar.series"></apexchart>
+    </div>
+
+    <div>
+      <h4 class="titleChartStyle">Chart User's Age</h4>
+      <apexchart @click="handleOnclick()" width="380" type="donut" :options="chartDataDonut.chartOptions" :series="chartDataDonut.series"></apexchart>
+    </div>
     <div>
     </div>
   </div>
@@ -27,21 +31,19 @@ export default {
   data() {
     return {
       url: "",
-      isLoading: false,
-
-      hover: ''
+      isLoading: false
     };
   },
 
   computed: {
-    Men() {
+    men() {
       var personsList = this.$store.getters["usersStore/getUsers"];
       var menList = personsList.filter(item => item.gender === "male");
       var allMen = menList.length;
 
       return allMen;
     },
-    Women() {
+    women() {
       var personsList = this.$store.getters["usersStore/getUsers"];
       var femaleList = personsList.filter(item => item.gender === "female");
       var allWomen = femaleList.length;
@@ -49,15 +51,65 @@ export default {
       return allWomen;
     },
 
-    chartData() {
+
+    ageOver30() {
+      var personsList = this.$store.getters["usersStore/getUsers"];
+      var usersOver30 = personsList.filter(item => item.age > 30);
+      var over30 = usersOver30.length;
+
+      return over30;
+    },
+
+    ageUnder30() {
+      var personsList = this.$store.getters["usersStore/getUsers"];
+      var usersUnder30 = personsList.filter(item => item.age <= 30);
+      var under30 = usersUnder30.length;
+      
+      return under30;
+    },
+
+    chartDataBar() {
       return {
-        series: [this.Men, this.Women],
+        chartOptions: {
+          chart: {
+            width: 500,
+            type: "bar"
+          },
+          xaxis: {
+            categories: ["MEN", "WOMEN"]
+          },
+
+          responsive: [
+            {
+              breakpoint: 480,
+              options: {
+                chart: {
+                  width: 200
+                },
+                legend: {
+                  position: "bottom"
+                }
+              }
+            }
+          ]
+        },
+        series: [
+          {
+            name: "series-1",
+            data: [this.men, this.women]
+          }
+        ]
+      };
+    },
+    chartDataDonut() {
+      return {
+        series: [this.ageOver30, this.ageUnder30],
         chartOptions: {
           chart: {
             width: 380,
             type: "donut"
           },
-          labels: ["MEN", "WOMEN"],
+          labels: ["Age >30", "Age <30"],
           responsive: [
             {
               breakpoint: 480,
@@ -106,11 +158,13 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.donutStyle {
-}
 /deep/ .apexcharts-canvas {
   margin: 0 auto;
   margin-top: 50px;
   cursor: pointer;
+}
+.titleChartStyle {
+  margin: 30px 0 40px 0;
+  text-transform: uppercase;
 }
 </style>
