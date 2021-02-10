d07753da98fe5154d009b55e62cbb71a4c75e5bd
Monica Piras <monica.piras.info@gmail.com>
2021-02-10 10:36:13 +0100

    update changes

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,7 @@
         <navigation-component/>
       </b-form-group>
 
-      <button type="button" aria-label="Close" class="close text-dark">
+      <button type="button" aria-label="Close" class="close closeStyle text-dark">
         <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="x" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-x b-icon bi">
           <g>
             <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"></path>
@@ -153,7 +153,7 @@ body {
   max-width: 80% !important;
 }
 
-.close {
+.closeStyle {
   position: absolute;
   top: 20px;
   right: -45px;
