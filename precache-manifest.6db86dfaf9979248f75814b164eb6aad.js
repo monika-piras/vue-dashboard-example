e8c0f242da147929dadea9fbe579d5e5f4f53c22
Monica Piras <monica.piras.info@gmail.com>
2022-07-27 10:28:06 +0200

    build branch for online demo

---
+++ precache-manifest.6db86dfaf9979248f75814b164eb6aad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47bcd94abe553d48ab07",
+    "url": "/css/app.d6a5dcc8.css"
+  },
+  {
+    "revision": "5954dff1b79142fee22f",
+    "url": "/css/chunk-vendors.f24b5491.css"
+  },
+  {
+    "revision": "c7638388138c9f519ec60f1b139da5d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47bcd94abe553d48ab07",
+    "url": "/js/app.4da63b8b.js"
+  },
+  {
+    "revision": "5954dff1b79142fee22f",
+    "url": "/js/chunk-vendors.7cf83fdc.js"
+  },
+  {
+    "revision": "af295a2b2410061b8f94a0579fb91f50",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
